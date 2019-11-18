1e5172af40932470b146dc8de98aedb6713cffa9
Vijay Yellepeddi <vijaysharma.yellepeddi@gmail.com>
2019-11-18 09:21:35 +0530

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt install build-essential python3 python3-pip
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+wasm-pack build
