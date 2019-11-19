b603d0012d359b3060911dea763f77dcc62f547a
Vijay Yellepeddi <vijaysharma.yellepeddi@gmail.com>
2019-11-19 07:55:23 +0530

    Changes to webpack code

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function add(a, b){
-    return a + b;
-}
+const wasm = import("./pkg");
+
+(() => {
+    wasm.then(instance => {
+        console.log(instance.add(2,3));
+    })
+})();
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn slow_func(){
+pub fn slow_func() -> f32{
     let mut result: f32 = 0.0;
     let upperbound: i32 = 10;
 	for i in (0..upperbound.pow(7)).rev() {
         result = result + ((i as f32).tan() * (i as f32).atan());
     }
+    result
 }
+
+#[wasm_bindgen]
+pub fn add(x: i32, y: i32) -> i32 { x + y }
